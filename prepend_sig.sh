ff2d4153a76647fbcb37728f51fc21076e6df732
Marcel Guzik <mguzik555@gmail.com>
2019-10-03 10:12:15 +0200

    Prepared script to prepend signature comment

---
+++ prepend_sig.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+DATE="$(date)"
+AUTHOR="Marcel Guzik"
+GROUP="środa parzysta"
+
+
+# TODO: replace naive first line check with regex expression,
+# fixing the command if invalid
+FILENAME=$1
+if [ "$FILENAME" = "" ]
+then
+    echo "gimme the name of the file ya meatbag"
+    exit
+fi
+
+if [ "$(head -n1 $FILENAME)" != '/*\n' ]
+then
+    CREDS="\tAutor: $AUTHOR\n\tGrupa: $GROUP\n\tData: $DATE\n"
+    COMMENT="/*\n$CREDS*/\n"
+    printf "$COMMENT\n%s" "$(cat $FILENAME)" > $FILENAME
+fi
+
+
+
